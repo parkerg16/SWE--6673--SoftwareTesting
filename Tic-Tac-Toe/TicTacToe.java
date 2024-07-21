bba7de1fc74b08aded1dd11a6c892e2af1f7e3fc
agouch <148926371+agouch@users.noreply.github.com>
2024-07-21 17:48:38 -0400

    Update TicTacToe.java

--- Tic-Tac-Toe/TicTacToe.java
+++ Tic-Tac-Toe/TicTacToe.java
@@ -1,12 +1,127 @@
-import java.awt.AWTException;
-import java.awt.Robot;
-import java.awt.event.KeyEvent;
-import java.io.IOException;
 import java.util.Scanner;
+import java.awt.Robot;
+import java.io.IOException;
+import java.awt.event.KeyEvent;
+import java.awt.AWTException;
+import java.util.concurrent.TimeUnit;
 
-class TicTacToe
-{
-	
+public class TicTacToe{
+	public char[][] board;
+    public char currentPlayer;
+    public boolean gameOver;
+
+    public TicTacToe() {
+        board = new char[3][3];
+        currentPlayer = 'X';
+        initializeBoard();
+        gameOver = false;
+    }
+	public void initializeBoard() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                board[i][j] = '-';
+            }
+        }
+    }
+
+    public char[][] getBoard() {
+        return board;
+    }
+
+    public boolean placeMark(int row, int col) {
+        if (row >= 0 && row < 3 && col >= 0 && col < 3 && board[row][col] == '-' && !gameOver) {
+            board[row][col] = currentPlayer;
+            if (checkForWin()) {
+                gameOver = true;
+            } else if (isBoardFull()) {
+                gameOver = true;
+            } else {
+                changePlayer();
+            }
+            return true;
+        }
+        return false;
+    }
+
+    public boolean checkForWin() {
+        return checkRowsForWin() || checkColumnsForWin() || checkDiagonalsForWin();
+    }
+
+    private boolean checkRowsForWin() {
+        for (int i = 0; i < 3; i++) {
+            if (checkRowCol(board[i][0], board[i][1], board[i][2])) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean checkColumnsForWin() {
+        for (int i = 0; i < 3; i++) {
+            if (checkRowCol(board[0][i], board[1][i], board[2][i])) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean checkDiagonalsForWin() {
+        return ((checkRowCol(board[0][0], board[1][1], board[2][2])) || (checkRowCol(board[0][2], board[1][1], board[2][0])));
+    }
+
+    private boolean checkRowCol(char c1, char c2, char c3) {
+        return (c1 != '-' && c1 == c2 && c2 == c3);
+    }
+
+    public boolean isBoardFull() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (board[i][j] == '-') {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public void changePlayer() {
+        currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
+    }
+
+    public char getCurrentPlayer() {
+        return currentPlayer;
+    }
+
+    public boolean isDraw() {
+        return isBoardFull() && !checkForWin();
+    }
+
+    public void resetGame() {
+        initializeBoard();
+        currentPlayer = 'X';
+        gameOver = false;
+    }
+
+    public boolean isGameOver() {
+        return gameOver;
+    }
+
+    public void printBoard() {
+        System.out.println("-------------");
+        for (int i = 0; i < 3; i++) {
+            System.out.print("| ");
+            for (int j = 0; j < 3; j++) {
+                System.out.print(board[i][j] + " | ");
+            }
+            System.out.println();
+            System.out.println("-------------");
+        }
+    }
+    public static void main() {
+        // Main method for manual testing
+        TicTacToe game = new TicTacToe();
+        game.printBoard();
+    }
 	public static void clearScreen()throws AWTException {  
       Robot rb = new Robot();
 	  rb.keyPress(KeyEvent.VK_C);
